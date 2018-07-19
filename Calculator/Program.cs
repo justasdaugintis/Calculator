107db80f07f6e89d64a9439fc88d358c11019cea
Justas Daugintis <>
2018-07-19 16:27:31 +0100

    adding more features

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,26 +13,40 @@ namespace Calculator
                 Console.WriteLine("Please enter the operator: ");
                 var operation = Console.ReadLine();
 
-                Console.WriteLine("Please enter the first number: ");
-                int firstNumber = int.Parse(Console.ReadLine());
-                Console.WriteLine("Please enter the second number: ");
-                int secondNumber = int.Parse(Console.ReadLine());
+                // create an array of numbers
+                Console.WriteLine("How many numbets you want to " + operation + " ?");
+                int numberAmount = int.Parse(Console.ReadLine());
+                int[] numbers= new int[numberAmount];
                 int sum = 0;
 
-                if (operation == "+")
-                    sum = firstNumber + secondNumber;
-                
-                else if(operation == "-" )
-                    sum = firstNumber - secondNumber;
-                
-                else if(operation == "*")
-                    sum = firstNumber * secondNumber;
+                // loop through each array item and give it value, then perform operations on those numbers.
 
-                else if(operation == "/")
-                    sum = firstNumber / secondNumber;
-                else
-                    Console.WriteLine("Something went wrong"); 
-                
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    Console.WriteLine("please enter numer " + (i + 1) + ":");
+                    numbers[i] = int.Parse(Console.ReadLine());
+                   
+                }
+                sum = numbers[0];
+
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if (operation == "+")
+                        sum = sum + numbers[i];
+
+                    else if (operation == "-")
+                        sum = sum - numbers[i];
+
+                    else if (operation == "*")
+                        sum = sum * numbers[i];
+
+                    else if (operation == "/")
+                        sum = sum / numbers[i];
+                    else
+                        Console.WriteLine("Something went wrong");
+                }
+
+                //Print out answer
                 Console.Write("The answer is: " + sum);
 
             }
